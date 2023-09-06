de620061015a352431cb11d18c678fd9652262c5
luis <itsmluis@gmail.com>
2023-09-06 14:55:44 -0600

    creation of the nav and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
