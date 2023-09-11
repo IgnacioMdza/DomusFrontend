e1f2b6c46ef9314c28f67cba0385d8fc92706f07
IgnacioMdza <mdza.ignacio@gmail.com>
2023-09-11 11:56:38 -0600

    styles and routes validations added in both bookingblog menu components, styles modified in booking page

--- components/BookingBlogDropdownMenu.jsx
+++ components/BookingBlogDropdownMenu.jsx
@@ -1,19 +1,20 @@
 import BarsMenu from '/public/icons/bars-menu.svg'
 import Link from 'next/link'
 import { useState } from 'react'
+import { useRouter } from 'next/router';
 import XMark from "/public/icons/xmark.svg";
 
 export default function BookingBlogDropdownMenu() {
 
+    const router = useRouter()
     const [isOpen, setIsOpen] = useState(false)
 
     return(
         <> 
             <div className='lg:hidden bg-[#FF7068] w-full flex flex-col items-center'>
-                {/* relative */}
                 <button 
                 onClick={() => setIsOpen((prev) => !prev)}
-                href='binnacle' 
+                href='Bookingblog' 
                 className='w-full py-[16px] px-[16px] flex gap-[20px] items-center place-content-center active:bg-[#d15751]'>
                     <p className='text-[20px] font-[nunito] text-[#F2F2F2]'>Menú</p>
                     {!isOpen ? <BarsMenu className='fill-[#F2F2F2] h-[24px] w-[24px]'/> : <XMark className='fill-[#F2F2F2] h-[24px] w-[24px]'/>}
@@ -21,12 +22,11 @@ export default function BookingBlogDropdownMenu() {
 
                 { isOpen && 
                     <div className='bg-white w-full shadow-2xl text-[#2B2E4A] flex flex-col text-center px-[12px] text-[20px] font-[nunito]'>
-                        {/* absolute top-[62px] */}
-                        <Link href='binnacle' className='border-b py-[12px] border-[#F2F2F2]'><p>Datos de reserva</p></Link>
-                        <Link href='binnacle' className='border-b py-[12px] border-[#F2F2F2]'><p>Mascotas</p></Link>
-                        <Link href='binnacle' className='border-b py-[12px] border-[#F2F2F2]'><p>Evidencia fotográfica</p></Link>
-                        <Link href='binnacle/message'className='border-b py-[12px] border-[#F2F2F2]'><p>Comunicación</p></Link>
-                        <Link href='binnacle' className='py-[12px] '><p>Reseñas</p></Link>
+                        <Link href={router.pathname === '/bookingblog' ? 'bookingblog/info' : 'info'} className='border-b py-[12px] border-[#F2F2F2]'><p>Datos de reserva</p></Link>
+                        <Link href={router.pathname === '/bookingblog' ? 'bookingblog/pets' : 'pets'} className='border-b py-[12px] border-[#F2F2F2]'><p>Mascotas</p></Link>
+                        <Link href={router.pathname === '/bookingblog' ? 'bookingblog/evidence' : 'evidence'} className='border-b py-[12px] border-[#F2F2F2]'><p>Evidencia fotográfica</p></Link>
+                        <Link href={router.pathname === '/bookingblog' ? 'bookingblog/chat' : 'chat'} className='border-b py-[12px] border-[#F2F2F2]'><p>Comunicación</p></Link>
+                        <Link href={router.pathname === '/bookingblog' ? 'bookingblog/reviews' : 'reviews'} className='py-[12px] '><p>Reseñas</p></Link>
                     </div>
                 }
             </div>
--- pages/bookingblog/index.jsx
+++ pages/bookingblog/index.jsx
@@ -16,17 +16,17 @@ export default function Bookingblog() {
                 </section>
                 <section className='bg-[#2B2E4A] w-full'>
                     <div className='flex flex-col lg:flex-row py-[24px] md:py-[40px] px-[16px] md:px-[24px] max-w-[1280px] mx-auto items-center gap-[40px] md:gap-[40px] min-h-[calc(100vh-202px)] md:min-h-[calc(100vh-250px)]'>
+                        <div className='flex flex-col gap-[28px] md:gap-[40px] lg:w-1/2 items-center'>
+                            <p className='text-[#F2F2F2] font-[nunito] text-[20px] md:text-[24px] text-center'>Este es un espacio donde puedes encontrar información relevante sobre tu reserva y seguir su curso.</p>
+                            <Link href='/profiles' className='text-[16px] md:text-[20px] font-[nunito] text-[#F2F2F2] p-[8px] text-center bg-[#FF7068] rounded-[12px] border-[4px] border-[#FF7068] hover:scale-[105%] hover:bg-opacity-50 transition shadow-xl w-full md:w-4/5'>Da click aquí para regresar a tu perfil</Link>
+                        </div>
                         <Image 
                             src={"/images/seccion_bitacora.png"}
                             alt="reviews_image"
                             height={2000}
                             width={2000}
-                            className='aspect-square md:w-10/12 lg:w-1/2 object-cover rounded-[48px] mx-auto bg-[#F2F2F2] border-solid border-[4px] md:border-[8px] border-[#F2F2F2]'
+                            className='aspect-square md:w-10/12 lg:w-1/2 object-cover rounded-full mx-auto bg-[#F2F2F2] border-solid border-[4px] md:border-[8px] border-[#F2F2F2]'
                         ></Image>
-                        <div className='flex flex-col gap-[28px] md:gap-[40px] lg:w-1/2 items-center'>
-                            <p className='text-[#F2F2F2] font-[nunito] text-[20px] md:text-[24px] text-center'>Este es un espacio donde puedes encontrar información relevante sobre tu reserva y seguir su curso.</p>
-                            <Link href='binnacle' className='text-[16px] md:text-[20px] font-[nunito] text-[#F2F2F2] p-[8px] text-center bg-[#FF7068] rounded-full border-[4px] border-[#FF7068] hover:scale-[105%] hover:bg-opacity-50 transition shadow-xl w-full md:w-5/6'>Da click aquí para regresar a tu perfil</Link>
-                        </div>
                     </div>
                 </section>
         </main>
