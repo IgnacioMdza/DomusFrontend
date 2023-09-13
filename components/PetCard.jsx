d3b5438fdd82dd6824983f58462a89a6aa4f9463
IgnacioMdza <mdza.ignacio@gmail.com>
2023-09-13 17:21:01 -0600

    Booking blog page created and styles modified in PetCard component

--- components/PetCard.jsx
+++ components/PetCard.jsx
@@ -6,8 +6,8 @@ const imageLoader = ({ src, width, quality }) => {
 
 export default function PetCard() {
   return (
-    <main className="px-5 py-3 bg-white rounded-[15px] w-full">
-      <div className="flex justify-around mb-4">
+    <main className="p-[16px] bg-white rounded-[15px] w-full shadow-md flex flex-col gap-[12px]">
+      <div className="flex items-center bg-[#F2F2F2] rounded-l-[72px] rounded-r-[72px] p-[8px]">
         <Image
           loader={imageLoader}
           alt="Profile Picture"
@@ -18,33 +18,37 @@ export default function PetCard() {
           height={110}
           className="object-cover rounded-full"
         />
-        <div className="text-center">
-          <p className="text-[Raleway] text-[30px] text-[#FF7068] mb-3">
+        <div className="mx-auto">
+          <p className="text-[Raleway] text-[28px] text-[#FF7068] mb-3 text-center">
             Rocko
           </p>
-          <p className="text-[25px]">⭐⭐⭐⭐⭐</p>
+          <p className="text-[18px]">⭐⭐⭐⭐⭐</p>
         </div>
       </div>
-      <p className="text-[16px] mb-5 text-center md:text-left">
+      <div className='flex justify-around w-full'>
+        <div className="inline-block text-[16px] text-center md:text-left">
+          <p className="font-bold text-center">
+            Raza: <span className="font-normal">Chihuahua</span>
+          </p>
+          <p className="font-bold text-center">
+            Edad: <span className="font-normal">2 años</span>
+          </p>
+        </div>
+        <div className="inline-block text-[16px] md:text-left">
+          <p className="font-bold text-center">
+            Sexo: <span className="font-normal">Macho</span>
+          </p>
+          <p className="font-bold text-center">
+            Tamaño: <span className="font-normal">Mediano</span>
+          </p>
+        </div>
+      </div>
+      <p className="text-[16px] text-center md:text-justify px-[4px]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
       </p>
-      <div className="inline-block w-[50%] text-[16px] text-center md:text-left">
-        <p className="font-bold">
-          Raza: <span className="font-normal">Chihuahua</span>
-        </p>
-        <p className="font-bold">
-          Edad: <span className="font-normal">2 años</span>
-        </p>
-      </div>
-      <div className="inline-block w-[50%] text-[16px] text-center md:text-left">
-        <p className="font-bold">
-          Sexo: <span className="font-normal">Macho</span>
-        </p>
-        <p className="font-bold">
-          Tamaño: <span className="font-normal">Mediano</span>
-        </p>
-      </div>
     </main>
   );
 }
