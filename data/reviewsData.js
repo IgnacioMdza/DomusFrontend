0235308b5e70c3cec293415991d08f0a23746b37
IgnacioMdza <mdza.ignacio@gmail.com>
2023-09-14 18:16:24 -0600

    Styles and content modified en ReviewCard component, M ui raiting was also added

--- data/reviewsData.js
+++ data/reviewsData.js
@@ -3,7 +3,7 @@ export const reviewsData = [
         authorImage: "https://randomuser.me/api/portraits/men/3.jpg",
         authorName: "Alberto Gutiérrez",
         reviewDate: "20/septiembre/2023",
-        value: "⭐⭐⭐⭐⭐",
+        value: 4.5,
         anfitrionName: 'Juan Cárdenas',
         review:
             "Fue mi primera vez utilizando esta aplicación y me he quedado con una muy buena experiencia.",
@@ -12,7 +12,7 @@ export const reviewsData = [
         authorImage: "https://randomuser.me/api/portraits/men/4.jpg",
         authorName: "Bernardo Castañeda",
         reviewDate: "12/agosto/2023",
-        value: "⭐⭐⭐⭐⭐",
+        value: 5,
         anfitrionName: 'Jazmín Lozano',
         review:
             "El cuidador fue muy atento y se ve que tiene mucha empatía con los animales, le doy 5 estrellas.",
@@ -21,7 +21,7 @@ export const reviewsData = [
         authorImage: "https://randomuser.me/api/portraits/women/5.jpg",
         authorName: "Karina González",
         reviewDate: "15/septiembre/2023",
-        value: "⭐⭐⭐⭐⭐",
+        value: 5,
         anfitrionName: 'Diana Pérez',
         review:
             "Muy amable el anfitrión, se mantuvo en constante comunicación conmigo.",
@@ -30,7 +30,7 @@ export const reviewsData = [
         authorImage: "https://randomuser.me/api/portraits/women/1.jpg",
         authorName: "Jazmín Ochoa",
         reviewDate: "10/agosto/2023",
-        value: "⭐⭐⭐⭐⭐",
+        value: 5,
         anfitrionName: 'Aylín Llerenas',
         review:
             "Excelente servicio, definitivamente volvería a dejar a mi mascota con el mismo anfitrión.",
@@ -39,7 +39,7 @@ export const reviewsData = [
         authorImage: "https://randomuser.me/api/portraits/women/10.jpg",
         authorName: "Laura Lozano",
         reviewDate: "01/octubre/2023",
-        value: "⭐⭐⭐⭐⭐",
+        value: 4.5,
         anfitrionName: 'Alberto Villa',
         review:
             "Quedé satisfecha con la estadía, mi mascota estuvo en buenas manos.",
@@ -48,7 +48,7 @@ export const reviewsData = [
         authorImage: "https://randomuser.me/api/portraits/men/9.jpg",
         authorName: "Rodrigo López",
         reviewDate: "08/septiembre/2023",
-        value: "⭐⭐⭐⭐⭐",
+        value: 5,
         anfitrionName: 'José Ramirez',
         review:
             "Un gran cuidador, seguramente lo volveré a escoger cuando necesite el servicio nuevamente.",
