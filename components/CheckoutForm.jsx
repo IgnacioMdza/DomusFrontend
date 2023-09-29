4018b3584c1b196a1e1a6fea5f3cc3439ec10e7a
IgnacioMdza <mdza.ignacio@gmail.com>
2023-09-29 16:23:54 -0600

    Reservation data card added to payment page

--- components/CheckoutForm.jsx
+++ components/CheckoutForm.jsx
@@ -19,15 +19,12 @@ export default function CheckoutForm() {
     if (!stripe) {
       return;
     }
-
     const clientSecret = new URLSearchParams(window.location.search).get(
       "payment_intent_client_secret"
     );
-
     if (!clientSecret) {
       return;
     }
-
     stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
       switch (paymentIntent.status) {
         case "succeeded":
@@ -53,7 +50,6 @@ export default function CheckoutForm() {
       
       return;
     }
-
     setIsLoading(true);
 
     const { error } = await stripe.confirmPayment({
@@ -93,18 +89,19 @@ export default function CheckoutForm() {
   };
 
   return (
-    <form id="payment-form" onSubmit={handleSubmit} className='w-3/5 bg-[#F2F2F2] p-[24px] rounded-xl shadow-xl'>
+    <form id="payment-form" onSubmit={handleSubmit} className='w-full lg:w-3/5 bg-[#F8F8F8] p-[24px] rounded-xl shadow-xl border-1 border-white'>
       {/* <LinkAuthenticationElement
         id="link-authentication-element"
         onChange={(e) => setEmail(e.target.value)}
       /> */}
+      <h1 className='mb-[12px] text-[32px] font-[Raleway] font-semibold text-[#2B2E4A]'>Pago</h1>
       <PaymentElement id="payment-element" options={paymentElementOptions} />
-      <button disabled={isLoading || !stripe || !elements} id="submit" className='bg-[#2B2E4A] font-bold border-[2px] border-[#2B2E4A] text-white p-[12px] w-full mt-[24px] rounded-full hover:scale-[101%] transition hover:bg-white hover:text-[#2B2E4A] hover:shadow-lg mb-1'>
+      <button disabled={isLoading || !stripe || !elements} id="submit" className='bg-[#2B2E4A] font-bold border-[2px] border-[#2B2E4A] text-white p-[10px] w-full mt-[24px] rounded-full hover:scale-[101%] transition hover:bg-white hover:text-[#2B2E4A] hover:shadow-lg mb-1'>
         <span id="button-text">
-          {isLoading ? <div className="spinner" id="spinner"></div> : "Pagar"}
+          {isLoading ? <div className="spinner" id="spinner"></div> : "Pagar reserva"}
         </span>
       </button>
-      {message && <div id="payment-message">{message}</div>}
+      {message && <div id="payment-message" className='text-[16px] w-full text-center mt-[12px]'>{message}</div>}
     </form>
   );
 }
--- pages/payment/index.js
+++ pages/payment/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { useEffect } from "react";
-
+import Link from "next/link";
 import CheckoutForm from "@/components/CheckoutForm";
-
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
@@ -13,8 +12,7 @@ export default function App() {
   const amount = 500
 
   useEffect(() => {
-    
-    fetch("http://localhost:8080/create-payment-intent", {
+    fetch("http://localhost:8080/payment/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ 
@@ -22,10 +20,11 @@ export default function App() {
         description: 'Pago personalizado',
         billing_details: {
           address: {
-            country: 'MX' // País del cliente
+            country: 'MX'
           }
-        }}),
-    })
+        },
+      reservationId: '651614867fcef96c1e4bbbd1'}),
+      })
       .then((res) => res.json())
       .then((data) => setClientSecret(data.clientSecret));
   }, []);
@@ -39,8 +38,33 @@ export default function App() {
   };
 
   return (
-    <main className="h-[calc(100vh-90px)] mt-[90px] bg-[#FF7068]">
-      <section className='max-w-[1024px] mx-auto p-[24px] flex place-content-center items-center h-full'>
+    <main className="mt-[90px] bg-[#FF7068]">
+      <section className='max-w-[1024px] mx-auto px-[24px] flex flex-col gap-[24px] place-content-center items-center min-h-[calc(100vh-90px)]'>
+        <div className='w-full lg:w-3/5 flex flex-col bg-[#F8F8F8] p-[24px] rounded-xl shadow-xl border-1 border-white'>
+          <div className='flex justify-between items-center'>
+            <h1 className='text-[32px] font-[Raleway] font-semibold text-[#FF7068]'>Reserva</h1>
+            <Link href="/" className='text-[16px] text-white bg-[#FF7068] px-[12px] py-[6px] rounded-full border border-[#FF7068] hover:shadow-md hover:scale-[102%] hover:bg-[#F2F2F2] transition hover:text-[#FF7068]'>Regresar</Link>
+          </div>
+          <p>Del 29 Sep 2023 al 01 Sep 2023</p>
+          <div className='flex flex-col gap-[4px] rounded-lg lg:p-[8px]'>
+            <div className='flex justify-between lg:text-[14px] w-full'>
+              <p>Noches agendadas</p>
+              <p>3 x $200.00 : $600.00</p>
+            </div>
+            <div className='flex justify-between lg:text-[14px] w-full'>
+              <p>Tarifa Domus</p>
+              <p>$300.00</p>
+            </div>
+            <div className='flex justify-between lg:text-[14px] w-full'>
+              <p>Impuestos</p>
+              <p>$300.00</p>
+            </div>
+            <div className='flex justify-between text-[16px] lg:text-[20px] w-full font-semibold border-t border-[#2B2E4A]'>
+              <p>TOTAL</p>
+              <p>$1200.00 MXN</p>
+            </div>
+          </div>
+        </div>
         {clientSecret && (
           <Elements options={options} stripe={stripePromise}>
             <CheckoutForm />
