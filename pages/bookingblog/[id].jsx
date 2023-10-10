8e47a245e3011c2945c59344a9dbaea634c53341
IgnacioMdza <mdza.ignacio@gmail.com>
2023-10-10 16:58:20 -0600

    Validating reservation status

--- pages/bookingblog/[id].jsx
+++ pages/bookingblog/[id].jsx
@@ -17,7 +17,10 @@ export default function Bookingblog({ reservation }) {
                 <div className='flex flex-col lg:flex-row py-[24px] md:py-[40px] px-[16px] md:px-[24px] max-w-[1280px] mx-auto items-center gap-[40px] md:gap-[40px] min-h-[calc(100vh-202px)] md:min-h-[calc(100vh-250px)]'>
                     <div className='flex flex-col gap-[28px] md:gap-[40px] lg:w-1/2 items-center'>
                         <p className='text-[#F2F2F2] font-[nunito] text-[20px] md:text-[24px] text-center'>Este es un espacio donde puedes encontrar información relevante sobre tu reserva y seguir su curso.</p>
-                        <Link href={`/profiles/${reservation.data.client._id}`} className='text-[16px] md:text-[20px] font-[nunito] text-[#F2F2F2] p-[8px] text-center bg-[#FF7068] rounded-[12px] border-[3px] border-[#FF7068] hover:scale-[105%] hover:bg-opacity-50 transition shadow-xl w-full md:w-4/5'>Da click aquí para regresar a tu perfil</Link>
+                        {/* Pendiente: Al implementar token, modificar el href con id de token */}
+                        <Link href={`/profiles/${reservation.data.client._id}`} className='text-[16px] md:text-[20px] font-[nunito] text-[#F2F2F2] p-[8px] text-center bg-[#FF7068] rounded-[12px] border-[3px] border-[#FF7068] hover:scale-[105%] hover:bg-opacity-50 transition shadow-xl w-full md:w-4/5'>
+                            Da click aquí para regresar a tu perfil
+                        </Link>
                     </div>
                     <Image 
                         src={"/images/seccion_bitacora.png"}
@@ -37,11 +40,17 @@ export async function getServerSideProps(context) {
     const response = await fetch(`http://localhost:8080/reservations/${id}`);
     if (response.status === 200) {
         const reservation = await response.json();
-        return {
-            props: {
-                reservation,
-            },
-        };
+        if(reservation.data.status != 'paid' && reservation.data.status != 'current' && reservation.data.status != 'concluded') {
+            return {
+                notFound: true, 
+            };
+        } else {
+            return {
+                props: {
+                    reservation,
+                },
+            };
+        }
     } else {
         return {
             notFound: true, 
