6deb0b80ba80e5d4839079d3cac1ec7223be8d45
IgnacioMdza <mdza.ignacio@gmail.com>
2023-10-19 21:53:23 -0600

    Validations to set data, rendering or redirect

--- pages/accounts/petregister/[id].jsx
+++ pages/accounts/petregister/[id].jsx
@@ -25,16 +25,19 @@ export default function PetRegister() {
   useEffect(() => {
     const pathId = router.query.id;
     const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/");
+      return;
+    }
     if(pathId && token){
       const tokenInfo = JSON.parse(atob(token.split(".")[1]));
       const pathId = router.query.id;
       if (tokenInfo.id != pathId || tokenInfo.userType != "client") {
         router.push("/");
+      } else {
+        setToken(token);
       }
-      setToken(token);
-    } else {
-      router.push("/");
-    }
+    } 
   }, [router.query.id, router]);
 
   const onSubmit = (data) => {
